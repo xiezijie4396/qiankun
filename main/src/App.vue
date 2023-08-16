5998d87572523733e0d19aabb6ad6306f4644b6c
xiezijie4396 <775826360@qq.com>
2023-08-16 11:50:10 +0800

    feat: 使用display的方式实现多页签，子应用的路由事件会影响到主应用

--- main/src/App.vue
+++ main/src/App.vue
@@ -5,15 +5,56 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/vue2">vue2</router-link> |
       <router-link to="/vue3/">vue3</router-link> |
-      <router-link to="/react/">react</router-link> |
+      <!-- <router-link to="/react/">react</router-link> |
       <router-link to="/default/">default</router-link> |
       <router-link to="/vue2hash/">vue2hash</router-link> |
-      <router-link to="/vue3hash/">vue3hash</router-link>
+      <router-link to="/vue3hash/">vue3hash</router-link> -->
     </nav>
     <router-view />
-    <div id="container"></div>
+    <div id="container-vue2" v-show="$route.path.includes('/vue2/')"></div>
+    <div id="container-vue3" v-show="$route.path.includes('/vue3/')"></div>
   </div>
 </template>
+
+<script>
+import { loadMicroApp } from 'qiankun';
+
+const apps = [
+  {
+    name: 'vue2', // app name registered
+    entry: '//localhost:8001/child/vue2/',
+    container: '#container-vue2',
+    props: {
+      activeRule: '/main/vue2'
+    }
+  },
+  {
+    name: 'vue3', // app name registered
+    entry: '//localhost:8002/child/vue3/',
+    container: '#container-vue3',
+    props: {
+      activeRule: '/main/vue3'
+    }
+  },
+]
+
+export default {
+  mounted() {
+    // 优先加载当前的子项目
+    const path = this.$route.path;
+    const currentAppIndex = apps.findIndex(item => path.includes(item.name));
+    if(currentAppIndex !== -1){
+      const currApp = apps.splice(currentAppIndex, 1)[0];
+      apps.unshift(currApp);
+    }
+    // loadMicroApp 返回值是 app 的生命周期函数数组
+    const loadApps = apps.map(item => loadMicroApp(item))
+    // 当 tab 页关闭时，调用 loadApps 中 app 的 unmount 函数即可
+    console.log(loadApps)
+  }
+}
+
+</script>
 
 <style lang="less">
 #app {
--- main/src/main.ts
+++ main/src/main.ts
@@ -2,23 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { registerMicroApps, start } from 'qiankun';
-import getRegisterApp from './register'
-const isDev = !(process.env.NODE_ENV === 'production')
+// import { registerMicroApps, start } from 'qiankun';
+// import getRegisterApp from './register'
+// const isDev = !(process.env.NODE_ENV === 'production')
 
-const registerArr = getRegisterApp()
-registerMicroApps(registerArr)
+// const registerArr = getRegisterApp()
+// registerMicroApps(registerArr)
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-start({
-  // 本地开发时，禁用预加载，防止因其他项目未启动而导致的报错
-  prefetch: isDev ? false : true,
-  // prefetch: false,
-  sandbox: {
-    experimentalStyleIsolation: true
-  }
-})
+// start({
+//   // 本地开发时，禁用预加载，防止因其他项目未启动而导致的报错
+//   prefetch: isDev ? false : true,
+//   // prefetch: false,
+//   sandbox: {
+//     experimentalStyleIsolation: true
+//   }
+// })
 
 new Vue({
   router,
